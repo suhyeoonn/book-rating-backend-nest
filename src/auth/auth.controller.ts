3ee705f81145e3e610c7285566df290b020a27e7
suhyeoonn <suhyeon1740@gmail.com>
2024-11-25 08:01:14 +0900

    🩹 login API 반환값 수정 및 스웨거에 표시

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Get,
+  HttpCode,
   Post,
   Req,
   Res,
@@ -13,6 +14,7 @@ import { UserDto, UserResponse } from './dto/user.dto';
 import { Response } from 'express';
 import { AuthGuard } from './security/auth.guard';
 import { User } from './entity/user.entity';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -29,20 +31,31 @@ export class AuthController {
   /**
    * 로그인 시 JWT 토큰을 생성하고 이를 쿠키에 저장합니다. (하루동안 유효)
    */
+  @ApiResponse({
+    status: 200,
+    description: 'Login Successful',
+    type: UserResponse,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Authentication Failed',
+  })
   @Post('/login')
+  @HttpCode(200)
   async login(@Body() userDto: UserDto, @Res() res: Response) {
     const result = await this.authService.validateUser(userDto);
     res.cookie('jwt', result.accessToken, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 1d
     });
-    return res.send(result);
+    return res.send({ user: result.user });
   }
 
   /**
    * 로그아웃
    */
   @Post('/logout')
+  @HttpCode(200)
   logout(@Res() res: Response) {
     res.cookie('jwt', '', {
       maxAge: 0,
